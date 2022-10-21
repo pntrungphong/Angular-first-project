62dde1052157c888a82c9b6aa3688c1646e42381
pntrungphong <phamnguyentrungphong@gmail.com>
2022-10-21 16:44:05 +0700

    first init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-assignment';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
